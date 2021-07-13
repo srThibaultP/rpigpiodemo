dc6512b4a674f450bf84838e0cddfb5122443f19
Thibault Pech <Thibault.Pech@rabaud.com>
2021-07-13 13:41:28 +0200

    Essai fix compteur

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
+let i = 0;
 
 console.log("En marche");
 button.watch((err, value) => {
     i++
-    if (i>1) {
-        i--;
-        console.log(new Date());
-    }
 });
+if (i>1) {
+    i--;
+    console.log(new Date());
+}
