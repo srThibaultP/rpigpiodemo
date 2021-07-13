4ce5a62ab38abbc19932b62db0b246502065db61
Thibault Pech <Thibault.Pech@rabaud.com>
2021-07-13 14:35:26 +0200

    Fichiers renommés, essai nouveau calcul secondes

--- EnvoiMessageApres2ndImpulsion.js
+++ EnvoiMessageApres2ndImpulsion.js
@@ -1,18 +1,18 @@
+//Envoi d'un message dans la console NodeJS après la reception de l'impulsion retour
+
 const Gpio = require('onoff').Gpio;
 const led = new Gpio(17, 'out');
 const button = new Gpio(4, 'in', 'both');
 let i = 0;
-let dateF = new Date().getTime() / 1000
 
 console.log("En marche");
 button.watch((err, value) => {
     i++;
     if (i == 2) {
-        date2 = dateF;
+        console.log("heya");
         i = 0;
-        console.log(date2 - date1);
     } else {
-        date1 = dateF;
+        console.log("nop");
     }
 });
 
--- dev.js
+++ dev.js
@@ -1,18 +1,1 @@
-const Gpio = require('onoff').Gpio;
-const led = new Gpio(17, 'out');
-const button = new Gpio(4, 'in', 'both');
-let i = 0;
-let dateF = Intl.DateTimeFormat('fr', { hour: "numeric", minute: "numeric", second: "numeric" }).format(new Date());
-
-console.log("En marche");
-button.watch((err, value) => {
-    i++;
-    if (i == 2) {
-        date2 = dateF;
-        i = 0;
-        console.log(date2 - date1);
-    } else {
-        date1 = dateF;
-    }
-});
-
+console.log(new Date().getTime() / 1000)
